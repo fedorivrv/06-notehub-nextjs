25060a994ffcc9df06d1a3b940a67de05e38a3f7
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2025-11-08 11:39:41 +0200

    fix

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,23 +1,5 @@
-import { QueryClient, dehydrate } from '@tanstack/react-query';
 import NoteDetailsClient from './NoteDetails.client';
-import { fetchNoteById } from '@/lib/api';
 
-interface NoteDetailsPageProps {
-  params: { id: string };
-}
-
-export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['note', params.id],
-    queryFn: () => fetchNoteById(params.id),
-  });
-
-  return (
-    <NoteDetailsClient
-      noteId={params.id}
-      dehydratedState={dehydrate(queryClient)}
-    />
-  );
+export default function NoteDetailsPage() {
+  return <NoteDetailsClient />;
 }
