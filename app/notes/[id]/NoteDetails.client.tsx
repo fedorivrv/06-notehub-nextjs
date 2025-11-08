f1411102f48da2b073657505f69fd3182cab8a8c
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2025-11-08 11:38:06 +0200

    fix

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import { useParams } from 'next/navigation';
 import css from './NoteDetails.module.css';
 
 export default function NoteDetailsClient() {
   const params = useParams();
-  const id = params.id;
+  const id = Array.isArray(params.id) ? params.id[0] : params.id;
 
   const { data: note, isLoading, error } = useQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id!),
+    enabled: !!id, // запит виконується тільки якщо id існує
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
@@ -24,7 +25,8 @@ export default function NoteDetailsClient() {
           <h2>{note.title}</h2>
         </div>
         <p className={css.content}>{note.content}</p>
-        <p className={css.date}>{note.createdAt}</p>
+        <p className={css.date}>Created: {new Date(note.createdAt).toLocaleString()}</p>
+        {note.tag && <p className={css.tag}>Tag: {note.tag}</p>}
       </div>
     </div>
   );
